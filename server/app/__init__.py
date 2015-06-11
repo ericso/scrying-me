0e0b7aa508a20fd56ef02b0764da720135a6b258
Eric So <me@eric.so>
2015-06-11 13:45:22 -0700

    Add template, cleanup

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
-  from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
-  # Define the WSGI application object
-  app = Flask(__name__)
+# Define the WSGI application object
+app = Flask(__name__)
 
-  # Configurations
-  app.config.from_object('config')
+# Configurations
+app.config.from_object('config')
 
-  # Define the database object which is imported
-  # by modules and controllers
-  db = SQLAlchemy(app)
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
 
 
-  # Put views import after app creation to avoid circular import
-  from app import views, models
-  
+# Put views import after app creation to avoid circular import
+from app import views, models
