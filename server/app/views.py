4173e0ab8d18f431fb13d896de61506fe34dfc1d
Eric So <me@eric.so>
2015-06-12 10:59:59 -0700

    GET user route

--- server/app/views.py
+++ server/app/views.py
@@ -5,6 +5,8 @@ from app.models import User
 
 @app.route('/api/users', methods = ['POST'])
 def new_user():
+  """API endpoint for creating a new user
+  """
   username = request.json.get('username')
   password = request.json.get('password')
 
@@ -17,8 +19,20 @@ def new_user():
   user.hash_password(password)
   db.session.add(user)
   db.session.commit()
-  # return jsonify({'username': user.username}), \
-  #   201, \
-  #   {'Location': url_for('get_user', id=user.id, _external=True)}
+  return jsonify({'username': user.username}), \
+    201, \
+    {'Location': url_for('get_user', id=user.id, _external=True)}
+
+
+@app.route('/api/users/<int:id>', methods=['GET'])
+def get_user(id):
+  """API endpoint for getting a user by username
+  """
+  if id is None:
+    abort(400) # missing arguments
+
+  user = User.query.get(id)
+  if user is None:
+    abort(400) # no user found
 
   return jsonify({'username': user.username}), 201
