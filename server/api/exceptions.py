9f7bdcd6672a8e4085a2b30daf63e5f9a981fae6
Eric So <me@eric.so>
2015-06-23 16:14:36 -0700

    Removed custom exceptions

--- server/api/exceptions.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify
-
-from common.exceptions import BaseException
-
-
-class InvalidAPIUsage(BaseException):
-
-  def __init__(self, message, status_code=400, payload=None):
-    BaseException.__init__(self, message, status_code, payload)
